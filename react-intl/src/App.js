ab63f30ba9273fc9f2c01ec9ba56c561ae4eb416
Hatice KATRANCI AKGÜL <76768140+haticekatranciakgul@users.noreply.github.com>
2024-04-17 21:37:13 +0300

    react intl

---
+++ react-intl/src/App.js
@@ -0,0 +1,42 @@
+import { title } from 'process';
+import './App.css';
+import { useState } from 'react';
+
+import { IntlProvider, FormattedMessage, FormattedNumber } from 'react-intl'
+
+const messages = {
+  'tr-TR': {
+    title: "Merhaba Dünya",
+    description: '3 yeni mesajınız var'
+
+  },
+  'en-US':{
+    title: "Hello World",
+    description: 'You have 3 messages'
+
+
+  }
+
+}
+
+function App() {
+  const [lang, setLang] = useState("tr-TR");
+  return (
+    <div className="App">
+
+      <IntlProvider messages={messages[lang]}>
+      <FormattedMessage id="title"/>
+        <p>
+          <FormattedMessage id="description"/>
+        </p>
+        <br></br>
+        <br></br>
+        <button onClick={() => setLang('tr-TR')}>TR</button>
+        <button onClick={() => setLang('en-US')}>EN</button>
+      </IntlProvider>
+
+    </div>
+  );
+}
+
+export default App;
